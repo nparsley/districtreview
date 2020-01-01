0b8a3e4450de6c73aa3ea1a9e9d979dcb4fdff80
Nick Parsley <nicholasparsley@gmail.com>
2020-01-01 09:31:38 +0000

    APIs updated with map showing

--- db/migrate/20200101085444_alter_shares_add_lat_and_lng.rb
+++ db/migrate/20200101085444_alter_shares_add_lat_and_lng.rb
@@ -1,4 +1,4 @@
-class AlterPlacesAddLatAndLng < ActiveRecord::Migration[5.2]
+class AlterSharesAddLatAndLng < ActiveRecord::Migration[5.2]
   def change
     add_column :shares, :latitude, :float
     add_column :shares, :longitude, :float
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_231928) do
+ActiveRecord::Schema.define(version: 2020_01_01_085444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
